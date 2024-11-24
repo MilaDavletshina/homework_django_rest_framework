b1bf8c5d06d9d476bf701f95325bcfa530c5920e
ludmila davletshina <ludaplv@mail.ru>
2024-11-24 12:58:00 +0300

    добавлены модели

---
+++ materials/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Название курса", help_text="Укажите курс"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание курса",
+        help_text="Укажите описание курса",
+    )
+    image = models.ImageField(
+        upload_to="materials/image",
+        blank=True,
+        null=True,
+        verbose_name="Фото",
+        help_text="Загрузите фото",
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Курсы"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=100, verbose_name="Название урока", help_text="Укажите урок"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание урока",
+        help_text="Укажите описание урока",
+    )
+    preview = models.ImageField(
+        upload_to="materials/preview",
+        blank=True,
+        null=True,
+        verbose_name="Фото",
+        help_text="Загрузите фото",
+    )
+    video_link = models.CharField(max_length=200)
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Курс",
+        help_text="Укажите курс",
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
