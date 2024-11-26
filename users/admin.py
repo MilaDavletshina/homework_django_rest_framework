3ae5a9102617505862a701b9fd6ae2d9803241c8
ludmila davletshina <ludaplv@mail.ru>
2024-11-26 19:58:14 +0300

    добавлен payment

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Payments
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "email")
+    list_display = ("id", "email", "first_name", "last_name", "phone", "city", "avatar")
+
+
+@admin.register(Payments)
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ("user", "payment_date", "paid_course", "paid_lesson", "payment_amount", "payment_method")
+    search_fields = ("user",)
+    search_filter = ("payment_date","paid_course", "paid_lesson",)
