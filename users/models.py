13b929757efe1c10908508f33a3f24b52fb8f02d
ludmila davletshina <ludaplv@mail.ru>
2024-11-26 19:58:42 +0300

    добавлена новая модель payment

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -37,3 +39,21 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payments(models.Model):
+    """ Платежи """
+    PAYMENT_METHOD_CHOICES = [
+        ('CASH', 'Наличные'),
+        ('TRANSFER', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="payments",verbose_name="пользователь")
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата оплаты")
+    paid_course = models.ForeignKey(Course, null=True, blank=True, on_delete=models.CASCADE)  # Связь с курсом
+    paid_lesson = models.ForeignKey(Lesson, null=True, blank=True, on_delete=models.CASCADE)  # Связь с уроком
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма оплаты")
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES, verbose_name="Способ оплаты")
+
+    def __str__(self):
+        return f'{self.user} - {self.amount} - {self.payment_date}'
