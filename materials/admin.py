af3cadc75428c37e7fe74485b8d38632615c3732
ludmila davletshina <ludaplv@mail.ru>
2024-11-26 19:58:25 +0300

    добавлен course, lesson

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Course, Lesson
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "image")
+    search_fields = ("name",)
+    search_filter = ("name",)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "preview", "video_link")
+    search_fields = ("name",)
+    search_filter = ("name",)
