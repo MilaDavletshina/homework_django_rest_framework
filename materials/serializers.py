aa09a800bd19e3fead29e1a9a9fb376809f3de34
ludmila davletshina <ludaplv@mail.ru>
2024-11-27 10:26:03 +0300

    добавлен метод для получения количество уроков курса и информацию по всем урокам курса одновременно

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,19 +3,24 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lesson = serializers.SerializerMethodField()
 
     def get_lesson_count(self, obj):
         """Получает объект курса obj и возвращает количество связанных уроков"""
         return obj.lessons.count()  # Используем related_name 'lessons'
 
+    def get_lesson(self, obj):
+        """Выдавет и количество уроков курса и информацию по всем урокам курса одновременно"""
+        return obj.lesson()
+
     class Meta:
         model = Course
         fields = ['name', 'lesson_count']  # указанные здесь поля будут возвращаться в postman
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
