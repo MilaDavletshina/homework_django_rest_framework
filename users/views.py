d97ebb3d4b75aa0c05cd3001b436c6447ddf663f
ludmila davletshina <ludaplv@mail.ru>
2024-11-27 11:13:44 +0300

    UserViewSet,  PaymentsViewSet

--- users/views.py
+++ users/views.py
@@ -1,1 +1,25 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
+from rest_framework.viewsets import ModelViewSet
+
+from users.models import User, Payments
+from users.serializers import UserSerializer, PaymentsSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PaymentsViewSet(ModelViewSet):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method',)
+    ordering_fields = ('payment_date',)
+
+#фильтровать по курсу или уроку,
+#фильтровать по способу оплаты.
