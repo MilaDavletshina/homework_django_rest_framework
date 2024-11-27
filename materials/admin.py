0d15c732dbe5146466101fc4c18d43298613f09b
ludmila davletshina <ludaplv@mail.ru>
2024-11-27 09:44:13 +0300

    добавлен admin.register для materials

--- materials/admin.py
+++ materials/admin.py
@@ -5,13 +5,13 @@ from materials.models import Course, Lesson
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "image")
+    list_display = ("id", "name", "description", "image")
     search_fields = ("name",)
     search_filter = ("name",)
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "preview", "video_link")
+    list_display = ("id", "title", "description", "preview", "video_link")
     search_fields = ("name",)
     search_filter = ("name",)
