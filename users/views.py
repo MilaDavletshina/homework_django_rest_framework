22d05a2567767010e31688b152b58b0ce65b413d
ludmila davletshina <ludaplv@mail.ru>
2024-11-30 10:08:56 +0300

    переопределен метод def perform_create

--- users/views.py
+++ users/views.py
@@ -17,6 +17,11 @@ class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
 
 class PaymentsViewSet(ModelViewSet):
     queryset = Payments.objects.all()
