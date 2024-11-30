98953e9be6bbc77cd16909d6421043f9ce35a0d1
ludmila davletshina <ludaplv@mail.ru>
2024-11-30 10:09:12 +0300

    добавлен путь для login, token

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,23 @@
-from django.urls import path
-from rest_framework.routers import SimpleRouter
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from users.apps import UsersConfig
 from users.views import PaymentsViewSet, UserViewSet, UserCreateAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register(r"user", UserViewSet, basename="user")
 router.register(r"payments", PaymentsViewSet, basename="payments")
 
 urlpatterns = [
-    path("users/create/", UserCreateAPIView.as_view(), name="users_create"),
+    path('', include(router.urls)),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login",),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh",),
 ]
 
 urlpatterns += router.urls
