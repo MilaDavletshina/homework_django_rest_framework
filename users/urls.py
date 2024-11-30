c32d1958ed03804fe356abf26d3c5fd81d19cf83
ludmila davletshina <ludaplv@mail.ru>
2024-11-30 08:42:26 +0300

    добавлен путь для UserCreate в urls

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from users.apps import UsersConfig
-from users.views import PaymentsViewSet, UserViewSet
-
+from users.views import PaymentsViewSet, UserViewSet, UserCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -10,6 +10,8 @@ router = SimpleRouter()
 router.register(r"user", UserViewSet, basename="user")
 router.register(r"payments", PaymentsViewSet, basename="payments")
 
-urlpatterns = []
+urlpatterns = [
+    path("users/create/", UserCreateAPIView.as_view(), name="users_create"),
+]
 
 urlpatterns += router.urls
