59892816eae264e489a21ef6eabc4b41f358145e
ludmila davletshina <ludaplv@mail.ru>
2024-12-04 15:56:50 +0300

    добавлена проверка наличия подписки в серилизатор CourseSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.validators import validate_video_url
 
 
@@ -17,10 +17,16 @@ class CourseSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(
         many=True, read_only=True
     )  # Выдает и количество уроков курса и информацию по всем урокам курса одновременно
+    is_subscription = serializers.SerializerMethodField()
 
     def get_lesson_count(self, obj):
         """Получает объект курса obj и возвращает количество связанных уроков"""
         return obj.lessons.count()  # Используем related_name 'lessons'
+
+    def get_is_subscription(self, obj):
+        """Проверяет наличие подписки."""
+        user = self.context['request'].user
+        return Subscription.objects.filter(user=user, course=obj).exists()
 
     class Meta:
         model = Course
@@ -28,4 +34,5 @@ class CourseSerializer(serializers.ModelSerializer):
             "name",
             "lesson_count",
             "lessons",
+            "is_subscription",
         ]  # указанные здесь поля будут возвращаться в postman
