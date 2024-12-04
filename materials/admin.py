54e1fd36ed617b5fce8a60ee69d880303e73902f
ludmila davletshina <ludaplv@mail.ru>
2024-12-04 13:46:49 +0300

    Подписка  добавлена на панель администратора

--- materials/admin.py
+++ materials/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 
 
 @admin.register(Course)
@@ -15,3 +15,10 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "description", "preview", "video_link")
     search_fields = ("name",)
     search_filter = ("name",)
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "course")
+    search_fields = ("user",)
+    search_filter = ("user",)
