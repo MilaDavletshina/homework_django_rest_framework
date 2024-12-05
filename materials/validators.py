5877464caa2046e3aa715df84314c66660e0ae2b
ludmila davletshina <ludaplv@mail.ru>
2024-12-05 16:15:54 +0300

    изменен  validator

--- materials/validators.py
+++ materials/validators.py
@@ -2,7 +2,9 @@ from rest_framework.serializers import ValidationError
 
 
 def validate_video_url(value):
-    if value.lower() != "www.youtube.com" and value.lower() != "youtube.com":
+    if "youtube.com" not in value.lower():
         raise ValidationError(
             "Cсылки на сторонние образовательные платформы или личные сайты — запрещены!"
         )
+
+
