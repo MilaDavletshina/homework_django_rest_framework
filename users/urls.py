8de59800ae5d243c11b13e6176d7e7817030ed28
ludmila davletshina <ludaplv@mail.ru>
2024-12-09 20:01:19 +0300

    добавлены  в urls

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentsViewSet, UserCreateAPIView, UserViewSet
+from users.views import PaymentsViewSet, UserCreateAPIView, UserViewSet, PaymentCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -25,6 +25,7 @@ urlpatterns = [
         TokenRefreshView.as_view(permission_classes=(AllowAny,)),
         name="token_refresh",
     ),
+    path("payment/", PaymentCreateAPIView.as_view, name="payment"),
 ]
 
 urlpatterns += router.urls
