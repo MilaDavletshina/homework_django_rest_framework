aee1464a8e9777790da73ebeec8d15049f2e6ac6
ludmila davletshina <ludaplv@mail.ru>
2024-12-09 13:57:53 +0300

    добавлены поля к модели payments

--- users/models.py
+++ users/models.py
@@ -69,6 +69,19 @@ class Payments(models.Model):
     payment_method = models.CharField(
         max_length=10, choices=PAYMENT_METHOD_CHOICES, verbose_name="Способ оплаты"
     )
+    session_id = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="ID сессии",
+        help_text="Укажите ID сессии", )
+    link = models.URLField(
+        max_length=400,
+        blank=True,
+        null=True,
+        verbose_name="Ссылка на оплату",
+        help_text="Укажите ссылку на оплату",
+    )
 
     def __str__(self):
         return f"Payment by {self.user} for {self.paid_course or self.paid_lesson}"
