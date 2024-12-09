4505238ff71c4977ff17308a1a723f24aecf9447
ludmila davletshina <ludaplv@mail.ru>
2024-12-09 20:01:01 +0300

    добавлены функции в services

--- users/services.py
+++ users/services.py
@@ -5,25 +5,28 @@ from config.settings import STRIPE_API_KEY
 stripe.api_key = STRIPE_API_KEY
 
 
-def create_stripe_price(amount):
+def create_stripe_product(course):
+    """Создание продукта в stripe."""
+    product = stripe.Product.create(name=course.name)
+    return product
+
+
+def create_stripe_price(amount, product_id):
     """Создание цены в stripe."""
-    stripe.Price.create(
+    price = stripe.Price.create(
         currency="rub",
         unit_amount=amount * 100,
-        product_data={"name": "Payments"},
+        product=product_id,
     )
+    return price
 
 
-def create_stripe_product(product):
-    """Создание продукта в stripe."""
-    stripe.Product.create(name=product)
-
-
-def create_stripe_sessions(price):
+def create_stripe_sessions(price_id):
     """Создание сессии в stripe."""
     sessions = stripe.checkout.Session.create(
+        payment_method_types=["card"],
         success_url="https://127.0.0.1:8000/",
-        line_items=[{"price": price.get("id"), "quantity": 1}],
+        line_items=[{"price": price_id, "quantity": 1}],
         mode="payment",
     )
     return sessions.get("id"), sessions.get("url")
