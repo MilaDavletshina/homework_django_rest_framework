97260efbded3be98fe18dfa5036295ae2fb53298
ludmila davletshina <ludaplv@mail.ru>
2024-12-09 20:01:10 +0300

    добавлены функции в views

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from requests import Response
+from rest_framework import filters, status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
+from materials.models import Course
 from users.models import Payments, User
 from users.serializers import PaymentsSerializer, UserSerializer
+from users.services import create_stripe_price, create_stripe_product, create_stripe_sessions
 
 
 class UserViewSet(ModelViewSet):
@@ -40,3 +43,22 @@ class PaymentsViewSet(ModelViewSet):
         "payment_method",
     )
     ordering_fields = ("payment_date",)
+
+
+class PaymentCreateAPIView(CreateAPIView):
+    """Оплата курса через stripe"""
+
+    queryset = User.objects.all()
+    serializer_class = PaymentsSerializer
+
+    def post(self, request):
+        course_id = request.data.get('course_id')
+        amount = request.data.get('amount')
+        course = Course.objects.get(id=course_id)
+
+        product = create_stripe_product(course)
+        price = create_stripe_price(amount, product.id)
+
+        session_id, payment_link = create_stripe_sessions(price)
+
+        return Response({"session_id": session_id, "payment_Link": payment_link}, status=status.HTTP_201_CREATED)
