567b9f26af8e8290a1dfd892f0ece9e2dd6dbb44
ludmila davletshina <ludaplv@mail.ru>
2024-12-14 14:03:28 +0300

    задача обновлена

--- materials/tasks.py
+++ materials/tasks.py
@@ -2,12 +2,15 @@ from config.settings import EMAIL_HOST_USER
 from django.core.mail import send_mail
 from celery import shared_task
 
+from materials.models import Subscription
+
 
 @shared_task
-def send_information_about_course_update(email):
+def send_information_about_course_update(course_id):
     """Отправляет сообщение пльзователю о подписке."""
     subject = "Обновление курса!"
     message = "Информируем Вас о том, что материалы курса обновлены!"
-
-    send_mail(subject, message, EMAIL_HOST_USER, [email])
+    subscription = Subscription.objects.filter(course_id=course_id)
+    for s in subscription:
+        send_mail(subject, message, EMAIL_HOST_USER, [s.user.email])
 
